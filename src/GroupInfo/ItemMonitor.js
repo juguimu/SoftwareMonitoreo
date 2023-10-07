9c81cebdf71881ded9d49f73402ff599203ab6f1
Juan Munoz <juangmunozc@gmail.com>
2023-10-06 19:26:06 -0500

    add figures and data monitor

--- src/GroupInfo/ItemMonitor.js
+++ src/GroupInfo/ItemMonitor.js
@@ -13,7 +13,7 @@ function ItemMonitor(
                 
                     
                 
-                const stringmonitorData={"variables":["V_A","I_A","PA_A","EA_A_I","F","FP_A"],"isActive":true,"clientGroupMonitorId":"74f9ae48-36ce-4b4e-bdd3-3899fa886b02.796b2cc2-b848-4e16-bf17-ff506b5d0602","measures":[{"timestamp":1695008874,"values":[130.3,0,0.5,0.006,59.9,1]},{"timestamp":1695012471,"values":[131.3,0,0.5,0.062,59.9,1]},{"timestamp":1695016056,"values":[131.3,1.45,118.9,0.115,59.9,0.62]}],"clientId":"1053779590"};    
+                const stringmonitorData={"variables":["V_A","I_A","PA_A","EA_A_I","F","FP_A"],"isActive":true,"clientGroupMonitorId":"74f9ae48-36ce-4b4e-bdd3-3899fa886b02.796b2cc2-b848-4e16-bf17-ff506b5d0602","measures":[{"timestamp":1695008874,"values":[130.3,0,0.5,0.006,59.9,1]},{"timestamp":1695012471,"values":[131.3,0,0.5,0.062,59.9,1]},{"timestamp":1695016056,"values":[131.3,1.45,118.9,0.115,59.9,0.62]},{"timestamp":1695016056,"values":[131.3,1.45,118.9,0.115,59.9,0.62]},{"timestamp":1695016056,"values":[131.3,1.45,118.9,0.115,59.9,0.62]},{"timestamp":1695016056,"values":[131.3,1.45,118.9,0.115,59.9,0.62]},{"timestamp":1695016056,"values":[131.3,1.45,118.9,0.115,59.9,0.62]},{"timestamp":1695016056,"values":[131.3,1.45,118.9,0.115,59.9,0.62]}],"clientId":"1053779590"};    
                 props.setdataMonitor(stringmonitorData);
                 
                         // console.log(data);
--- src/MonitorInfo/index.js
+++ src/MonitorInfo/index.js
@@ -2,11 +2,95 @@ import React from 'react';
 import './MonitorInfo.css';
 import { ItemData } from './ItemData';
 
+import {Bar} from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler
+);
+
 
 function MonitorInfo(
     {showWindow,
     dataMonitor}
    ){   
+
+
+    let timestamp = dataMonitor.measures.map((value) => new Date(value.timestamp*1000).toLocaleTimeString());
+
+   let paData = {
+    labels: timestamp,
+    datasets: [ // Cada una de las líneas del gráfico
+        {
+            label: 'Potencia [W]',
+            data: dataMonitor.measures.map((value) => value.values[2])
+        }
+    ]
+   };
+
+   let eaData = {
+    labels: timestamp,
+    datasets: [ // Cada una de las líneas del gráfico
+        {
+            label: 'Energía Consumida [kW/h]',
+            data: dataMonitor.measures.map((value) => value.values[3])
+        }
+    ]
+   };
+
+   let fpData = {
+    labels: timestamp,
+    datasets: [ // Cada una de las líneas del gráfico
+        {
+            label: 'Factor de potencia',
+            data: dataMonitor.measures.map((value) => value.values[5])
+        }
+    ]
+   };
+
+   let mioptions = {
+    
+    };
+
+    // promedios
+    
+    let Va = dataMonitor.measures.map((value) => value.values[0]);
+
+    let Vaprom = Va.reduce((acum,x)=>acum+x,0)/Va.length;
+
+    let Ia = dataMonitor.measures.map((value) => value.values[1]);
+
+    let Iaprom = Ia.reduce((acum,x)=>acum+x,0)/Ia.length;
+
+    let f = dataMonitor.measures.map((value) => value.values[4]);
+
+    let fprom = f.reduce((acum,x)=>acum+x,0)/f.length;
+
+    
+
+    
+
+
+
+
+
     return (
         <div className={`data-visualization ${showWindow.monitorInfoWindow}`}>
             <div> 
@@ -44,8 +128,74 @@ function MonitorInfo(
                         />
                 )}
 
+                <button onClick={()=>{
+
+                    let timestamp = dataMonitor.measures.map((value) => value.timestamp);
+
+
+
+                    console.log(new Date(timestamp[0]).toLocaleDateString());// dos decimales
+
+                }}>Pruebas</button>
+
+                
+
+                {/* {dataMonitor.measures.map(value =>
+                        <ItemData
+                            key={value.timestamp}
+                            timestamp={value.timestamp}
+                            values={value.values}
+                        />
+                )} */}
+
 
             </div>
+            <div className='figures-data'>
+                    <div className='item-figure'>
+                        <Bar  
+                            data = {paData}
+                            options = {mioptions} 
+                        />
+                    </div>
+                    <div className='item-figure'>
+                        <Bar 
+                            data = {eaData}
+                            options = {mioptions} 
+                        />
+                    </div>
+                    <div className='item-figure'>
+                        <Bar  
+                            data = {fpData}
+                            options = {mioptions} 
+                        />
+                    </div>                         
+            </div>
+
+            <div className='valuesboxes-data'>
+                <div className='data-box'>
+                    <p>Voltaje Efectivo RMS</p>
+                    <div>
+                        <p>{`${Vaprom.toFixed(2)}V`}</p>
+                    </div>
+                </div>
+
+                <div className='data-box'>
+                    <p>Corriente RMS</p>
+                    <div>
+                        <p>{`${Iaprom.toFixed(2)}A`}</p>
+                    </div>
+                </div>
+
+                <div className='data-box'>
+                    <p>Frecuencia</p>
+                    <div>
+                        <p>{`${fprom.toFixed(2)}Hz`}</p>
+                    </div>
+                </div>
+
+            </div>
+
+
         </div>
     );
 }
